44ae2e9b2fb783c5d0cfe73adb25d00d877d4dc4
Maya <maya@circleci.com>
2022-07-03 13:46:50 -0400

    updated solution

--- Problems/Arrays/MergeSortedArray.js
+++ Problems/Arrays/MergeSortedArray.js
@@ -4,16 +4,13 @@ function mergeSortedArrayDv(a1, a2) {
     var mergedPointer = 0;
     var mergedArray = [];
     while (a1Pointer < a1.length || a2Pointer < a2.length) {
-        if (a1Pointer < a1.length && a1[a1Pointer] < a2[a2Pointer]) {
+        if (a2Pointer >= a2.length || a1[a1Pointer] < a2[a2Pointer]) {
             mergedArray[mergedPointer++] = a1[a1Pointer++];
         }
-        else if (a2Pointer < a2.length) {
+        else {
             mergedArray[mergedPointer++] = a2[a2Pointer++];
-        }
-        else if (a1Pointer < a1.length) {
-            mergedArray[mergedPointer++] = a1[a1Pointer++];
         }
     }
     return mergedArray;
 }
-console.log(mergeSortedArrayDv([0, 3, 4, 8, 9], [4, 6, 7]));
+console.log(mergeSortedArrayDv([0, 3, 4, 8], [0, 6, 30]));
--- Problems/Arrays/MergeSortedArray.ts
+++ Problems/Arrays/MergeSortedArray.ts
@@ -6,9 +6,9 @@ function mergeSortedArrayDv(a1: number[], a2: number[])
     let a2Pointer = 0;
     let mergedPointer = 0;
     const mergedArray: number[] = [];
-    while(a1Pointer < a1.length && a2Pointer < a2.length)
+    while(a1Pointer < a1.length || a2Pointer < a2.length)
     {
-        if(a1[a1Pointer] < a2[a2Pointer])
+        if(a2Pointer >= a2.length || a1[a1Pointer] < a2[a2Pointer])
         {
             mergedArray[mergedPointer++] = a1[a1Pointer++]
         }
@@ -17,15 +17,7 @@ function mergeSortedArrayDv(a1: number[], a2: number[])
             mergedArray[mergedPointer++] = a2[a2Pointer++]
         }
     }
-    while(a1Pointer < a1.length)
-    {
-        mergedArray[mergedPointer++] = a1[a1Pointer++]
-    }
-    while(a2Pointer < a2.length)
-    {
-        mergedArray[mergedPointer++] = a2[a2Pointer++]
-    }
     return mergedArray;
 }
 
-console.log(mergeSortedArrayDv([0,3,4,8],[4,6,30]));
+console.log(mergeSortedArrayDv([0,3,4,8],[0,6,30]));
