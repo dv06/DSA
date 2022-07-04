ac663aa9a4b9fff17597f2d7f448d96f61837467
Maya <maya@circleci.com>
2022-07-04 00:24:22 -0400

    using class

--- Problems/Google/minStack.js
+++ Problems/Google/minStack.js
@@ -1,46 +1,34 @@
-
-var MinStack = function() {
-    this.value = null;
-    this.rest = null;
-    this.min = null;
-};
-
-/** 
- * @param {number} val
- * @return {void}
- */
-MinStack.prototype.push = function(val) {
-    const stack = new MinStack();
-    stack.value = this.value;
-    stack.rest = this.rest;
-    stack.min = this.min;
-    this.value = val;
-    this.rest = stack;
-    this.min = this.min != null ? Math.min(this.min, val) : val;
-};
-
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-    this.value = this.rest.value;
-    this.min = this.rest.min;
-    this.rest = this.rest.rest;
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-    return this.value;
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-    return this.min;
-};
+class MinStack {
+    constructor() {
+        this.value = null;
+        this.rest = null;
+        this.min = null;
+    }
+    
+    push(val) {
+        const stack = new MinStack();
+        stack.value = this.value;
+        stack.rest = this.rest;
+        stack.min = this.min;
+        this.value = val;
+        this.rest = stack;
+        this.min = this.min != null ? Math.min(this.min, val) : val;
+    }
+    
+    pop() {
+        this.value = this.rest.value;
+        this.min = this.rest.min;
+        this.rest = this.rest.rest;
+    }
+    
+    top() {
+        return this.value;
+    }
+    
+    getMin() {
+        return this.min;
+    }
+}
 
 /** 
  * Your MinStack object will be instantiated and called as such:
